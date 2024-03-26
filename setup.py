f52a55ab33ffaedcac179a848bfd2a3244ac97a9
Gourang <gourangpatidar2003@gmail.com>
2024-03-26 17:02:32 +0530

    template updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+__version__="0.0.0.0"
+
+REPO_NAME="End-To-End-Text-Summarizer"
+AUTHOR_NAME="GourangPatidar"
+SRC_REPO="TextSummarizer"
+AUTHOR_NAME="gourangpatidar2003@gmailcom"
+
