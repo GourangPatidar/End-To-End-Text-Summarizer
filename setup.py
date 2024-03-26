f1dc7b6768c6c939f241ef65fb9403ce2ebfff03
Gourang <gourangpatidar2003@gmail.com>
2024-03-26 17:07:25 +0530

    setup completed

--- setup.py
+++ setup.py
@@ -6,7 +6,23 @@ with open("README.md", "r") as fh:
 __version__="0.0.0.0"
 
 REPO_NAME="End-To-End-Text-Summarizer"
-AUTHOR_NAME="GourangPatidar"
+AUTHOR_USER_NAME="GourangPatidar"
 SRC_REPO="TextSummarizer"
-AUTHOR_NAME="gourangpatidar2003@gmailcom"
+AUTHOR_EMAIL="gourangpatidar2003@gmailcom"
 
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
